5ad2cfc3d7e802f61301fc8a7ea507d1c97ba14f
JoeCelaster <joecelaster2006@gmail.com>
2026-04-29 11:33:09 +0530

    refactor:stable functions - moduled/clean index.js

---
+++ prompts/common/askPackageManager.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import gradient from "gradient-string";
+import figlet from "figlet";
+import ora from "ora";
+import boxen from "boxen";
+import fs from "fs";
+import path from "path";
+
+export async function askPackageManager() {
+  return await inquirer.prompt([
+    {
+      type: "list",
+      name: "packageManager",
+      message: "Choose a package manager:",
+      choices: [
+        { name: chalk.bold.red("npm"), value: "npm" },
+        { name: chalk.bold.cyan("yarn"), value: "yarn" },
+        { name: chalk.bold.yellow("pnpm"), value: "pnpm" },
+        { name: chalk.bold.white("bun"), value: "bun" },
+      ],
+      pageSize: 10,
+      default: detectPackageManager(),
+    },
+  ]);
+}
