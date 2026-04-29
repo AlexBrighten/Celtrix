d52569d39e049e3f26776440947b9649be9bc368
JoeCelaster <joecelaster2006@gmail.com>
2026-04-29 11:33:55 +0530

    refactor:stable-celtrix functions are moduled

---
+++ prompts/stable/detectPackageManager.js
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import gradient from "gradient-string";
+import figlet from "figlet";
+import ora from "ora";
+import boxen from "boxen";
+import fs from "fs";
+import path from "path";
+
+export function detectPackageManager() {
+  const userAgent = process.env.npm_config_user_agent;
+  if (!userAgent) return "npm";
+  if (userAgent.includes("pnpm")) return "pnpm";
+  if (userAgent.includes("yarn")) return "yarn";
+  if (userAgent.includes("bun")) return "bun";
+  if (userAgent.includes("npm")) return "npm";
+  return "npm";
+}
---
+++ prompts/stable/showBanner.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import gradient from "gradient-string";
+import figlet from "figlet";
+import ora from "ora";
+import boxen from "boxen";
+import fs from "fs";
+import path from "path";
+
+export function showBanner() {
+  console.log(
+    gradient.pastel(
+      figlet.textSync("Celtrix", {
+        font: "Big",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+      })
+    )
+  );
+  console.log(chalk.gray("⚡ Setup Web-apps in seconds, not hours ⚡\n"));
+}
